8d28872f06483abf15f289af68367f06760ee0a6
kelvinkipsang <kelvinkipsang@gmail.com>
2016-07-25 09:40:11 +0300

    updates on max and min function

--- maximum_and_minimum/Maximum_minimum.java
+++ maximum_and_minimum/Maximum_minimum.java
@@ -9,3 +9,28 @@
 // put the lowest in its array 
 // put the highest in its array
 // output the highest then output the lowest
+
+public class MaxAndMin {
+
+        /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numArray[] = new int[]{2, 1, 3,4,7,9};
+        int maxNum = numArray[0];
+        int minNum = numArray[0];
+        for(int iterate=0; iterate<numArray.length; iterate++){
+            if(numArray[iterate]> maxNum){
+                maxNum = numArray[iterate];
+            }
+            else if(numArray[iterate]< minNum){
+                minNum = numArray[iterate];
+            }
+        }
+          System.out.println("Maximum: " +" "+ maxNum);
+          System.out.println("Minimum: " +" "+ minNum);
+    }
+
+}
